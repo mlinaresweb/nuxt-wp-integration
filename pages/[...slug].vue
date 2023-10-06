2149cf69f78c24095cc33b68fcd1af80830bbc55
mlinaresweb <ghaedesigns@gmail.com>
2023-10-06 12:41:27 +0200

    yoast add

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -7,22 +7,37 @@ let slug = ref('home')  // valor por defecto
 if (route.params.slug) {
     let lastIndex = route.params.slug.length - 1
     slug.value = route.params.slug[lastIndex] || route.params.slug[lastIndex - 1];
-}
+};
 
 const loadComponent = (name) => {
     return defineAsyncComponent(() => import(`@/components/pages/${name}.vue`))
-}
+};
 
 const CurrentComponent = ref(loadComponent(slug.value));
 
 const { data, pending, error, refresh } = await useFetch('http://localhost/cochesdelujo/wp-json/wp/v2/pages', {
     query: { slug: slug.value }
 });
+
+useServerSeoMeta({
+    title: () => data.value[0].yoast_head_json.title,
+    description: () => data.value[0].yoast_head_json.description,
+    ogUrl: () => data.value[0].yoast_head_json.og_url,
+    ogTitle: () => data.value[0].yoast_head_json.og_title,
+    ogDescription: () => data.value[0].yoast_head_json.og_description,
+    ogImage: () => data.value[0].yoast_head_json.og_image,
+    ogType: () => data.value[0].yoast_head_json.og_type,
+    ogLocale: () => data.value[0].yoast_head_json.og_locale,
+    twitterCard: () => data.value[0].yoast_head_json.twitter_card,
+    twitterTitle: () => data.value[0].yoast_head_json.twitter_title,
+    twitterDescription: () => data.value[0].yoast_head_json.twitter_description,
+    twitterImage: () => data.value[0].yoast_head_json.twitter_image,
+});
 </script>
 
 <template>
 <div>
-    <component :is="CurrentComponent" v-if="CurrentComponent && data[0]" :data="data[0]"></component>
+    <component :is="CurrentComponent" v-if="CurrentComponent && data[0]" :data="data[0]"/>
     <p v-if="error">Error loading the page</p>
 </div>
 </template>
