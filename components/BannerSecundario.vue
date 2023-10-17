e988e45a90a8e1991f3da46c712bbc51e95c4f42
mlinaresweb <ghaedesigns@gmail.com>
2023-10-17 21:09:42 +0200

    fix

--- components/BannerSecundario.vue
+++ components/BannerSecundario.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="relative mb-12">
-      <img src="https://i.ibb.co/x3nkbV2/banner2.webp" alt="Banner" class="w-full h-auto" />
+      <img src="https://i.ibb.co/x3nkbV2/banner2.webp" alt="Banner" width="943" height="197" class="w-full h-auto" />
       <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black via-transparent to-transparent opacity-50"></div>
     </div>
   </template>
