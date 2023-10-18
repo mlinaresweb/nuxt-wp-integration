7d1ef2a9f9e6d84c325e0176a56dcd92cfbb6bb7
mlinaresweb <ghaedesigns@gmail.com>
2023-10-18 21:51:03 +0200

    img

--- components/BannerSecundario.vue
+++ components/BannerSecundario.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative mb-12 h-56 md:h-64 lg:h-72 xl:h-auto">
-    <NuxtImg src="/banner2.webp" alt="Banner" loading="lazy" sizes="(max-width: 640px) 600px, 1920px"  class="w-full h-full object-cover" />
+    <NuxtImg src="https://i.ibb.co/K9mnphf/banner22.webp" alt="Banner" loading="lazy" sizes="(max-width: 640px) 600px, 1920px"  class="w-full h-full object-cover" />
     <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black via-transparent to-transparent opacity-70"></div>
   </div>
 </template>
