7c89db7a9cc83674a049c42f805dc9c4bb6bed51
mlinaresweb <ghaedesigns@gmail.com>
2023-10-18 13:51:29 +0200

    footer mobile add

---
+++ components/FooterDesktop.vue
@@ -0,0 +1,61 @@
+<template>
+    <footer class="bg-luxury-blue text-white p-8">
+      <div class="container mx-16 grid grid-cols-4 2xl:gap-72 gap-6">
+        <div>
+            <div class="flex items-center mb-4">
+                <img src="/logo.png" alt="Logo" class="mr-4">
+                <h1 class="font-bold text-xl">Coches de Lujo</h1>
+            </div>
+          <div class="space-y-2">
+            <p>Contactános a través de correo</p>
+            <p>Lun. a Juev de 10.00 a 19.00</p>
+            <p>Vie. de 10.00 a 15.00</p>
+            <p>Email</p>
+            <p><a href="mailto:reservas@cochesdelujo.cat" >reservas@cochesdelujo.cat</a></p>
+            <p>LLámanos</p>
+            <p>(00) 112 365 489</p>
+          </div>
+        </div>
+  
+        <div>
+          <h1 class="font-bold text-xl mb-4">Nuestras sugerencias</h1>
+          <ul class="space-y-2">
+            <li><a href="#" >Alquiler Ferrari Barcelona</a></li>
+            <li><a href="#" >Alquiler Porsche Barcelona</a></li>
+            <li><a href="#">Alquiler Lamborghini Barcelona</a></li>
+            <li><a href="#" >Coches para bodas</a></li>
+            <li><a href="#" >Alquiler coche por hora</a></li>
+            <li><a href="#" >Visita guiada</a></li>
+          </ul>
+        </div>
+  
+        <div>
+          <h1 class="font-bold text-xl mb-4">Páginas</h1>
+          <ul class="space-y-2">
+            <li><a href="#" >Blog</a></li>
+            <li><a href="#" >Contacto</a></li>
+            <li><a href="#" >Nosotros</a></li>
+          </ul>
+        </div>
+  
+        <div class="ml-[-25px] xl:ml-0">
+          <h1 class="font-bold text-xl mb-4">Suscríbete</h1>
+          <input type="email" placeholder="Email" class="p-2 w-auto mb-4 bg-transparent text-white rounded-lg border-white">
+          <div class=" flex flex-wrap gap-4 text-center">
+          <a
+            href="#"
+            class="block rounded-lg bg-white px-6 py-3 text-sm font-medium text-black shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+          >
+            Enviar
+          </a>
+        </div>
+        </div>
+      </div>
+      
+      <div class="mt-10 border-t pt-4 text-right -mb-3">
+        <a href="#" class="mr-4">Terms and conditions</a>
+        <a href="#" class="mr-4">Legal Note</a>
+        <a href="#" class="mr-4">Privacy</a>
+      </div>
+    </footer>
+  </template>
---
+++ components/FooterMobile.vue
@@ -0,0 +1,63 @@
+<template>
+    <footer class="bg-luxury-blue text-white p-4 ">
+      <div class="container mx-auto">
+        <div class="flex flex-col space-y-4">
+          <div class="flex justify-between">
+            <div>
+              Contáctanos a través de correo de Lun. a Juv. de 10:00 a 19:00 y Vie. de 10:00 a 15:00
+            </div>
+           
+          </div>
+          <div>
+              Email
+              <br>
+              email@cochesdelujo.cat
+            </div>
+          <div>
+            Llámanos
+            <br>
+            (00) 112 365 489
+          </div>
+  
+          <div class="flex flex-col space-y-2">
+            <input type="email" placeholder="Email" class="p-2 w-auto mb-4 bg-transparent text-white rounded-lg border-white">
+            <div class=" flex flex-wrap gap-4 text-center">
+          <a
+            href="#"
+            class="block rounded-lg bg-white px-6 py-3 text-sm font-medium text-black shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+          >
+            Enviar
+          </a>
+        </div>
+          </div>
+  
+          <div class="flex justify-between">
+            <div class="flex flex-col space-y-2">
+              <h3 class="text-lg">Nuestras sugerencias</h3>
+              <a href="#" class="text-sm">Alquiler Ferrari Barcelona</a>
+              <a href="#" class="text-sm">Alquiler Porsche Barcelona</a>
+              <a href="#" class="text-sm">Alquiler Lamborghini Barcelona</a>
+              <a href="#" class="text-sm">Coches para bodas</a>
+              <a href="#" class="text-sm">Alquiler coche por hora</a>
+              <a href="#" class="text-sm">Visita guiada</a>
+            </div>
+            <div class="flex flex-col space-y-2">
+                <h3 class="text-lg">Paginas</h3>
+              <a href="#" class="text-sm">Blog</a>
+              <a href="#" class="text-sm">Contacto</a>
+              <a href="#" class="text-sm">Nosotros</a>
+            </div>
+          </div>
+  
+          <div class="flex justify-center items-center pb-4 pt-4">
+                <img src="/logo.png" alt="Logo" class="mr-4">
+                <h1 class="font-bold text-xl">Coches de Lujo</h1>
+            </div>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
+  <style scoped>
+  </style>
+  
