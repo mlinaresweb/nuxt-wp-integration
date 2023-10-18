9a1a8d17c97cc95a11d2ea7c4d30d6f01d6c695a
mlinaresweb <ghaedesigns@gmail.com>
2023-10-18 19:20:46 +0200

    brands

--- components/CarBrands.vue
+++ components/CarBrands.vue
@@ -5,8 +5,14 @@
     <!-- En pantallas grandes -->
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6" v-if="isLargeScreen">
       <div v-for="brand in brands" :key="brand.name" class="flex flex-col items-center justify-center">
-        <div :class="`brand-image ${brand.name.toLowerCase()}`">
-          <img :src="brand.imageUrl" :alt="brand.name" class="max-w-full max-h-full object-contain">
+        <div :class="`brand-image aspect-ratio-box ${brand.name.toLowerCase()}`">
+          <img 
+    :src="brand.imageUrl" 
+    :alt="brand.name" 
+    :width="brand.width" 
+    :height="brand.height" 
+    class="max-w-full max-h-full object-contain"
+>
         </div>
         <span class="text-sm mt-2">{{ brand.name }}</span>
       </div>
@@ -17,7 +23,7 @@
     :space-between="10">
       <swiper-slide v-for="brand in brands" :key="brand.name">
         <div class="flex flex-col items-center justify-center">
-          <div :class="`brand-image ${brand.name.toLowerCase()}`">
+          <div :class="`brand-image aspect-ratio-box ${brand.name.toLowerCase()}`">
             <img :src="brand.imageUrl" :alt="brand.name" class="max-w-full max-h-full object-contain">
           </div>
           <span class="text-sm mt-2">{{ brand.name }}</span>
@@ -35,28 +41,30 @@
   type Brand = {
     name: string;
     imageUrl: string;
+    width: number;
+    height: number;
   };
   
   export default defineComponent({
     name: 'Brands',
     setup() {
       const brands = ref<Brand[]>([
-        { name: 'Ferrari', imageUrl: 'https://i.ibb.co/fNw2gbB/ferrari.webp' },
-        { name: 'Audi', imageUrl: 'https://i.ibb.co/cQDmjwP/audi.webp' },
-        { name: 'BMW', imageUrl: 'https://i.ibb.co/XyPjtFQ/bmw-1.webp' },
-        { name: 'Lamborghini', imageUrl: 'https://i.ibb.co/Rc2c0Vc/lamborghini.webp' },
-        { name: 'Mercedes', imageUrl: 'https://i.ibb.co/7Wc88j2/mercedes.webp' },
-        { name: 'Maserati', imageUrl: 'https://i.ibb.co/MndThg1/maserati.webp' },
-        { name: 'Tesla', imageUrl: 'https://i.ibb.co/x8NCtJx/tesla.webp' },
-        { name: 'Aston Martin', imageUrl: 'https://i.ibb.co/RyZtTHx/astonmartin-1.webp' },
-        { name: 'Mustang', imageUrl: 'https://i.ibb.co/n7yBpsd/mustang-1.webp' },
-        { name: 'McLaren', imageUrl: 'https://i.ibb.co/tZWTPMZ/mclaren.webp' },
-        { name: 'Porsche', imageUrl: 'https://i.ibb.co/gD4CPW2/porsche-1.webp' },
-        { name: 'Land Rover', imageUrl: 'https://i.ibb.co/MRXmSNT/landrover.webp' },
-        { name: 'Rolls Royce', imageUrl: 'https://i.ibb.co/GF29KDB/rollsroys.webp' },
-        { name: 'Jaguar', imageUrl: 'https://i.ibb.co/Bcs9Vth/jaguar.webp' },
-        { name: 'Bentley', imageUrl: 'https://i.ibb.co/61nnv4f/bentley.webp' },
-        { name: 'Hummer', imageUrl: 'https://i.ibb.co/vZGtk37/hummer-logo-1.webp' },
+        { name: 'Ferrari', imageUrl: 'https://i.ibb.co/fNw2gbB/ferrari.webp', width:36, height:45 },
+        { name: 'Audi', imageUrl: 'https://i.ibb.co/cQDmjwP/audi.webp', width:66, height:26  },
+        { name: 'BMW', imageUrl: 'https://i.ibb.co/XyPjtFQ/bmw-1.webp', width:45, height:45  },
+        { name: 'Lamborghini', imageUrl: 'https://i.ibb.co/Rc2c0Vc/lamborghini.webp', width:42, height:45  },
+        { name: 'Mercedes', imageUrl: 'https://i.ibb.co/7Wc88j2/mercedes.webp', width:43, height:45  },
+        { name: 'Maserati', imageUrl: 'https://i.ibb.co/MndThg1/maserati.webp', width:36, height:56  },
+        { name: 'Tesla', imageUrl: 'https://i.ibb.co/x8NCtJx/tesla.webp', width:47, height:45  },
+        { name: 'Aston Martin', imageUrl: 'https://i.ibb.co/RyZtTHx/astonmartin-1.webp', width:82, height:21  },
+        { name: 'Mustang', imageUrl: 'https://i.ibb.co/n7yBpsd/mustang-1.webp', width:76, height:34  },
+        { name: 'McLaren', imageUrl: 'https://i.ibb.co/tZWTPMZ/mclaren.webp', width:45, height:45  },
+        { name: 'Porsche', imageUrl: 'https://i.ibb.co/gD4CPW2/porsche-1.webp', width:35, height:45  },
+        { name: 'Land Rover', imageUrl: 'https://i.ibb.co/MRXmSNT/landrover.webp', width:68, height:39  },
+        { name: 'Rolls Royce', imageUrl: 'https://i.ibb.co/GF29KDB/rollsroys.webp', width:29, height:36  },
+        { name: 'Jaguar', imageUrl: 'https://i.ibb.co/Bcs9Vth/jaguar.webp', width:47, height:48  },
+        { name: 'Bentley', imageUrl: 'https://i.ibb.co/61nnv4f/bentley.webp', width:79, height:45  },
+        { name: 'Hummer', imageUrl: 'https://i.ibb.co/vZGtk37/hummer-logo-1.webp' , width:88, height:57 },
       ]);
       
       const screenWidth = ref<number>(0);  // valor inicial
