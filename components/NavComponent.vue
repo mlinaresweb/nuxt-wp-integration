8349b55afe651edee01189f32bf27deb664a2f41
mlinaresweb <ghaedesigns@gmail.com>
2023-10-19 16:15:06 +0200

    nav mobile

--- components/NavComponent.vue
+++ components/NavComponent.vue
@@ -3,17 +3,17 @@
       <div class="flex items-center justify-between w-full md:mx-16 lg:mx-16">
   
         <!-- Logo a la izquierda solo para móviles -->
-        <a href="#" class="md:hidden">
+        <a href="#" v-if="isMobile" >
           <NuxtImg src="https://i.ibb.co/T8FGxdn/logo.webp" alt="Logo"  width="22" height="33" class=""/>
         </a>
   
   <!-- Texto centrado para móviles, junto con logo para pantallas más grandes -->
   <div class="flex items-center justify-center space-x-4 md:justify-start w-full md:w-auto">
-        <NuxtImg src="https://i.ibb.co/T8FGxdn/logo.webp" alt="Logo"  width="22" height="33" class="hidden md:block"/> <!-- Oculta en móviles -->
+        <NuxtImg src="https://i.ibb.co/T8FGxdn/logo.webp" alt="Logo"  width="22" height="33" v-if="!isMobile"/> <!-- Oculta en móviles -->
         <span class="font-bold text-white">Coches de Lujo</span>
       </div>
  <!-- Menú de navegación para pantallas grandes -->
- <div class="hidden md:flex items-center space-x-8">
+ <div class="hidden md:flex items-center space-x-8" v-if="!isMobile">
             <a href="#" class="text-white">Inicio</a>
             <a href="#" class="text-white">Alquiler</a>
             <a href="#" class="text-white">Coches</a>
@@ -27,7 +27,7 @@
         </div>
 
         <!-- Icono de menú hamburguesa para pantallas móviles -->
-        <div class="flex md:hidden items-center space-x-4">
+        <div class="flex md:hidden items-center space-x-4" v-if="isMobile">
           <button @click="toggleMenu" class="focus:outline-none" :aria-label="isMenuOpen ? 'Cerrar menú' : 'Abrir menú'">
     <div class="w-6 h-0.5 bg-white mb-1.5"></div>
     <div class="w-6 h-0.5 bg-white mb-1.5"></div>
@@ -58,7 +58,7 @@
 
 
 <script setup lang="ts">
-import { ref, onMounted,watch } from 'vue';
+import { ref, onMounted, onBeforeUnmount, watchEffect, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
 import { useOutsideClick } from '@/composables/useOutsideClick';  
@@ -71,7 +71,7 @@ const selectedLanguage = ref(locale.value);
 const isMenuOpen = ref(false);  // Variable para controlar la apertura del menú
 const navRef = ref(null);
 const outsideClicked = useOutsideClick(navRef);
-
+const windowWidth = ref(0);
 const dynamicId = "language-selector";
 
 const labelForSelectedLanguage = computed(() => {
@@ -85,9 +85,6 @@ function changeLanguage() {
   if (router.currentRoute.value.path !== newPath) {
     router.push({ path: newPath });
   }
-
-  // Al cambiar el idioma, guárdalo en localStorage
-  // localStorage.setItem('selectedLanguage', selectedLanguage.value);
 }
 // Cierra el menú cuando se haga clic fuera de él
 watch(outsideClicked, (newValue) => {
@@ -98,12 +95,21 @@ watch(outsideClicked, (newValue) => {
 function toggleMenu() {
     isMenuOpen.value = !isMenuOpen.value;
 }
+onMounted(() => {
+  windowWidth.value = window.innerWidth;
+  window.addEventListener('resize', updateWidth);
+});
 
-// onMounted(() => {
-//   // Intenta obtener el idioma seleccionado de localStorage si estás en el lado del cliente
-//   const initialLanguage = process.client ? localStorage.getItem('selectedLanguage') || locale.value : locale.value;
-//   selectedLanguage.value = initialLanguage;
-// });
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateWidth);
+});
+
+function updateWidth() {
+  windowWidth.value = window.innerWidth;
+}
+
+const isMobile = computed(() => windowWidth.value <= 768);  
+
 </script>
 
 <style scoped>
