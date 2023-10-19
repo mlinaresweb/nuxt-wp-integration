ca23f285f1cd528ebc76bc6d8291647040c6091f
mlinaresweb <ghaedesigns@gmail.com>
2023-10-19 12:07:58 +0200

    lazy components

--- components/pages/home.vue
+++ components/pages/home.vue
@@ -1,22 +1,25 @@
 <template>
-    <div>
-      <HeaderComponent :title="data.title.rendered" :subtitle="data.content.rendered"/>
-      <!-- <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1>  -->
-      <LuxuryCarRental />
-      <InfoCar/>
-      <ServicesComponent/>
-      <Testimonials/>
-      <BannerSecundario/>
-      <CarBrands/>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import HeaderComponent from '@/components/HeaderComponent.vue';
-  
-  const props = defineProps({
-    data: Object
-  });
-  </script>
-  
+  <div>
+    <HeaderComponent :title="data.title.rendered" :subtitle="data.content.rendered" />
+    <LuxuryCarRental />
+    <InfoCar />
+    <ServicesComponent />
+    <Testimonials />
+    <BannerSecundario />
+    <CarBrands />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+const LuxuryCarRental = defineAsyncComponent(() => import('@/components/LuxuryCarRental.vue'));
+const InfoCar = defineAsyncComponent(() => import('@/components/InfoCar.vue'));
+const ServicesComponent = defineAsyncComponent(() => import('@/components/ServicesComponent.vue'));
+const Testimonials = defineAsyncComponent(() => import('@/components/Testimonials.vue'));
+const BannerSecundario = defineAsyncComponent(() => import('@/components/BannerSecundario.vue'));
+const CarBrands = defineAsyncComponent(() => import('@/components/CarBrands.vue'));
+
+const props = defineProps({
+  data: Object
+});
+</script>
